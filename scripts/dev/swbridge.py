bcf46facb2fca785291660a1ccf71de945422d0e
gu <facemanj@web.de>
2018-01-22 10:51:05 +0100

    Code out some warnings

--- scripts/dev/swbridge.py
+++ scripts/dev/swbridge.py
@@ -3,10 +3,9 @@
 from logging import basicConfig, debug, info, warning, DEBUG
 import selectors
 import socket
-from ril_h import ERRNO, ERRNO_NO_RESOURCES, REQUEST, UNSOL
+import ril_h as r
 
 
-# TODO should token be hex?
 # TODO make data mandatory
 ETH_P_ALL = 0x0003
 RILPROXY_PORT = 18912
@@ -86,12 +85,11 @@ class Dissector(object):
     # Inset custom rilproxy request constants
     REQUEST_SETUP = 0xc715
     REQUEST_TEARDOWN = 0xc717
-    REQUEST[REQUEST_SETUP] = "SETUP"
-    REQUEST[REQUEST_TEARDOWN] = "TEARDOWN"
+    r.REQUEST[REQUEST_SETUP] = "SETUP"
+    r.REQUEST[REQUEST_TEARDOWN] = "TEARDOWN"
 
     # Inset additional constant defined by ril.h
-    RIL_RESPONSE_ACKNOWLEDGEMENT = 800
-    REQUEST[RIL_RESPONSE_ACKNOWLEDGEMENT] = "RESPONSE_ACKNOWLEDGEMENT"
+    r.REQUEST[r.RESPONSE_ACKNOWLEDGEMENT] = "r.RESPONSE_ACKNOWLEDGEMENT"
 
     # Constants for response types defined by libril/ril.cpp
     RESPONSE_SOLICITED = 0
@@ -110,20 +108,19 @@ class Dissector(object):
 
     def dissect(self, bfr, source):
         '''Dissect the RIL packets and return a list of RIL message objects.'''
-        msg_len = len(bfr)
+        packet_len = len(bfr)
         self.packet_num += 1
         fmt_num = '\t[' + str(self.packet_num) + '] %s: %s'
         fmt_pkt = '\t[' + str(self.packet_num) + ']  %s:\t%s'
         fmt_none = '\t[' + str(self.packet_num) + '] %s'
 
-        debug(fmt_num, 'buffer length (raw)', msg_len)
+        debug(fmt_num, 'buffer length (raw)', packet_len)
 
         # Follow-up to a message where length header indicates more bytes than
         # available in the message.
-        print(self.cache)
         if self.bytes_missing > 0 and source in self.cache:
             self.cache[source] = b''.join([self.cache[source], bfr])
-            self.bytes_missing = self.bytes_missing - msg_len
+            self.bytes_missing = self.bytes_missing - packet_len
 
             debug(fmt_num, 'buffer length (reassembled)',
                   len(self.cache[source]))
@@ -135,7 +132,7 @@ class Dissector(object):
                 return []
 
             bfr = self.cache[source]
-            msg_len = len(bfr)
+            packet_len = len(bfr)
 
             del self.cache[source]
 
@@ -143,8 +140,8 @@ class Dissector(object):
         self.request_num = self.request_num + 1
 
         # Message must be at least 4 bytes
-        if msg_len < 4:
-            warning(fmt_num, 'dropping short buffer of length', msg_len)
+        if packet_len < 4:
+            warning(fmt_num, 'dropping short buffer of length', packet_len)
 
             return []
 
@@ -163,8 +160,8 @@ class Dissector(object):
             del self.cache[source]
 
             return []
-        if msg_len <= (header_len - 4):
-            self.bytes_missing = header_len - msg_len + 4
+        if packet_len <= (header_len - 4):
+            self.bytes_missing = header_len - packet_len + 4
             self.cache[source] = bfr
 
             debug(fmt_none, 'caching the package')
@@ -185,7 +182,8 @@ class Dissector(object):
         if (source == 'ril0'):
             debug(fmt_none, 'RIL REQUEST')
             debug(fmt_pkt, 'length', header_len)
-            debug(fmt_pkt, 'command', maybe_unknown(REQUEST, command_or_type))
+            debug(fmt_pkt, 'command', maybe_unknown(r.REQUEST,
+                                                    command_or_type))
             if (header_len > 4):
                 token = int.from_bytes(bfr[8:12], byteorder='little')
 
@@ -226,7 +224,7 @@ class Dissector(object):
 
                 error = int.from_bytes(bfr[12:16], byteorder='little')
 
-                debug(fmt_pkt, 'error', maybe_unknown(ERRNO, error))
+                debug(fmt_pkt, 'error', maybe_unknown(r.ERRNO, error))
 
                 request = self.requests[token]
                 request_delta = self.request_num - request['request_num']
@@ -236,15 +234,15 @@ class Dissector(object):
                 ril_msgs.append(ril_msg)
 
                 # NO_RESOURCES seems to retry again
-                if ril_msg.error != ERRNO_NO_RESOURCES:
+                if ril_msg.error != r.ERRNO_NO_RESOURCES:
                     if ril_msg.token in self.pending_requests:
                         self.pending_requests.remove(ril_msg.token)
                     else:
-                        # TODO try to make sure that warning disappears.. maybe
+                        # TODO try to make warning disappear.. maybe
                         # I am sometimes to slow and vendorril tries again?
                         warning(fmt_num, 'token already removed',
                                 ril_msg.token)
-                debug(fmt_pkt, 'command', maybe_unknown(REQUEST,
+                debug(fmt_pkt, 'command', maybe_unknown(r.REQUEST,
                                                         ril_msg.command))
                 debug(fmt_num, 'packets until reply', request_delta)
 
@@ -262,7 +260,7 @@ class Dissector(object):
 
                 command = int.from_bytes(bfr[8:12], byteorder='little')
 
-                debug(fmt_pkt, 'command', maybe_unknown(UNSOL, command))
+                debug(fmt_pkt, 'command', maybe_unknown(r.UNSOL, command))
 
                 ril_msg = RilUnsolicitedResponse(command, header_len)
                 ril_msgs.append(ril_msg)
@@ -285,9 +283,18 @@ class Dissector(object):
         info(fmt_num, 'In-flight requests', len(self.pending_requests))
 
         # If data is left in buffer, run dissector on it
-        # TODO RESPONSE_ACK Request seems to be padded with 6 additional bytes
-        # TODO SETUP Request seems to be padded with 10 additional bytes
-        if msg_len > header_len + 4:
+        len_diff = packet_len - (header_len + 4)
+        if len_diff > 0:
+            # SETUP request is padded with 10 bytes
+            if command_or_type == self.REQUEST_SETUP:
+                if len_diff == 10:
+                    return ril_msgs
+
+            # RESPONSE_ACKNOWLEDGEMENT request is padded with 6 bytes
+            if ril_msgs != []:
+                if ril_msgs[-1].command == r.RESPONSE_ACKNOWLEDGEMENT:
+                    if len_diff == 6:
+                        return ril_msgs
             info('..running dissector again..')
             additional_ril_msgs = self.dissect(bfr[header_len + 4:], source)
             for msg in additional_ril_msgs:
